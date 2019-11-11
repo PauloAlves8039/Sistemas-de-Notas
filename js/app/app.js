f8f3c0a039245fa3f83980fafde3010ffcf27a37
PauloAlves8039 <pj.alves547@gmail.com>
2019-11-11 20:01:37 -0300

    Commit inicial do repositório

---
+++ js/app/app.js
@@ -0,0 +1,6 @@
+/*
+ * Arquivo: app.js
+ * Autor: Paulo Alves
+ * Descrição: responsável por conter as informações principais da aplicação
+ * Data: 11/11/2019
+*/
---
+++ js/app/controllers.js
@@ -0,0 +1,7 @@
+/*
+ * Arquivo: controllers.js
+ * Autor: Paulo Alves
+ * Descrição: controlador responsável pela regra de negócio da aplicação e contato
+ * Data: 11/11/2019
+*/
+
