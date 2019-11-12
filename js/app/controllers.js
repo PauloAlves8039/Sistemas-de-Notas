ee49051484ac7aaa877572dfbcba2cc85267f575
PauloAlves8039 <pj.alves547@gmail.com>
2019-11-11 22:17:48 -0300

    Atualizando notas dos alunos

--- js/app/controllers.js
+++ js/app/controllers.js
@@ -9,12 +9,12 @@ angular.module("alunoModulo")
 .controller("indexController", function($scope){
     $scope.titulo = "Sistema de Notas"
     $scope.alunos = [
-        {nome: "Joana",email:"joana@mail.com",nota1:9.5,nota2:8.0,nota3:5.5},
-        {nome: "Fabiano",email:"fabiano@mail.com",nota1:7.0,nota2:4.5,nota3:6.5},
-        {nome: "Adriana",email:"adriana@mail.com",nota1:8.5,nota2:7.0,nota3:5.5},
-        {nome: "Carlos",email:"carlos@mail.com",nota1:7.5,nota2:8.0,nota3:6.5},
-        {nome: "Raquel",email:"raquel@mail.com",nota1:6.5,nota2:3.0,nota3:6.5},
-        {nome: "Marcelo",email:"marcelo@mail.com",nota1:6.5,nota2:6.0,nota3:7.5}
+        {nome: "Joana",email:"joana@mail.com",nota1:9.5,nota2:8.5,nota3:5.5},
+        {nome: "Fabiano",email:"fabiano@mail.com",nota1:7.5,nota2:4.5,nota3:6.5},
+        {nome: "Adriana",email:"adriana@mail.com",nota1:8.5,nota2:7.5,nota3:5.5},
+        {nome: "Carlos",email:"carlos@mail.com",nota1:7.5,nota2:8.5,nota3:6.5},
+        {nome: "Raquel",email:"raquel@mail.com",nota1:6.5,nota2:3.5,nota3:6.5},
+        {nome: "Marcelo",email:"marcelo@mail.com",nota1:6.5,nota2:6.5,nota3:7.5}
     ]
 
     let init = function(){
