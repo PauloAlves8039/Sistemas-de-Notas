c489095429a8f5786b84914420389b3005457572
PauloAlves8039 <pj.alves547@gmail.com>
2019-11-11 21:08:10 -0300

    Criando rotas para as views

--- js/app/app.js
+++ js/app/app.js
@@ -6,3 +6,13 @@
 */
 
 angular.module("alunoModulo",[''])
+
+.config(function($routeProvider){
+    $routeProvider
+    .when("/home", {
+        templateUrl:"templates/home.html", controller:"indexController"
+    })
+    .when("/contato", {
+        templateUrl:"templates/contato.html", controller:"contatoController"
+    })
+})
